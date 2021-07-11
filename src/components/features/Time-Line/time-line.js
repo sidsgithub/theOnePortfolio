c2fafb2ac407d215887c83991e5c0788e2ae211b
sidsgithub <emailmesidd@gmail.com>
2021-07-11 13:12:21 +0530

    basic structure - timeline

--- src/components/features/Time-Line/time-line.js
+++ src/components/features/Time-Line/time-line.js
@@ -9,60 +9,19 @@ import ThemeContext from "../../../context/theme-context";
 const TimeLine = () => {
   const { theme } = useContext(ThemeContext);
   return (
-    <div id={"timeline"} className="timeline-container">
-      <div className="timeline">
-        <div className="heading-container">
-          <BackgroundTag theme={theme} value={`<h3>`} />
-          <div className={`timeline-heading`}>Journey So Far </div>
-          <BackgroundTag theme={theme} value={`</h3>`} />
-        </div>
-        <div className="timelin-wrapper">
-          <div className="timeline">
-            <BackgroundTag theme={theme} value={`<section>`} />
-            <div>1. Institutions</div>
-            <BackgroundTag theme={theme} value={`</section>`} />
-            {education.map((element) => (
-              <div className="timeline-points">
-                <span className="dot"></span>
-                <hr />
-                <div className="element-wrap-container">
-                  <BackgroundTag theme={theme} value={`<cite>`} />
-                  <TimeLineElement
-                    img={element.img}
-                    schoolName={element.schoolName}
-                    eduTitle={element.eduTitle}
-                    date={element.date}
-                  />
-                  <BackgroundTag theme={theme} value={`</cite>`} />
-                </div>
-              </div>
-            ))}
+    <section className="timeline">
+      <ul>
+      {education.map(edu => 
+        <li>
+          <div>
+            <time>{edu.date}</time>
+            {edu.schoolName}
           </div>
-          <div className="timeline">
-            <BackgroundTag theme={theme} value={`<section>`} />
-            <div>2. Experiences</div>
-            <BackgroundTag theme={theme} value={`</section>`} />
-            {work.map((element) => (
-              <div className="timeline-points">
-                <span className="dot"></span>
-                <hr />
-                <div className="element-wrap-container">
-                  <BackgroundTag theme={theme} value={`<cite>`} />
-                  <TimeLineElement
-                    img={element.img}
-                    schoolName={element.schoolName}
-                    eduTitle={element.eduTitle}
-                    date={element.date}
-                  />
-                  <BackgroundTag theme={theme} value={`</cite>`} />
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-        {/** work */}
-      </div>
-    </div>
+        </li>
+        )}
+        
+      </ul>
+    </section>
   );
 };
 export default TimeLine;
